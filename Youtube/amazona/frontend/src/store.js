66cd496e5c154c9f9661dcae1860f92f12673307
Diego Apolinário <dieego.apolinario@gmail.com>
2021-02-09 23:44:01 -0300

    update (incompleto)

---
+++ Youtube/amazona/frontend/src/store.js
@@ -0,0 +1,19 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
+import { userSigninReducer } from './reducers/userReducers';
+
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: {cartItems} };
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+  userSignin: userSigninReducer,
+})
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhacer(applyMiddleware(thunk)));
+export default store;
---
+++ amazona-v1.0/frontend/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
+
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: {cartItems} };
+const reducer = combineReducers({
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer
+})
+const composeEnhacer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhacer(applyMiddleware(thunk)));
+export default store;
